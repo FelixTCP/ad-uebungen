be9bd85fa6830618da3e776ef5fb5b05f62312f5
Felix Wensky <felix.wensky@gmail.com>
2025-06-18 13:09:26 +0200

    kuh

---
+++ datastructures/src/stack.rs
@@ -0,0 +1,25 @@
+struct Stack<T> {
+    items: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack { items: Vec::new() }
+    }
+
+    fn push(&mut self, item: T) {
+        self.items.push(item);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.items.last()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+}
